161f2e8ce1e0a4d98234ab10ccdebd70e6301347
IvanTheTerrible <plagues.wtp@gmail.com>
2024-04-22 21:53:20 +0300

    adding new routes

---
+++ routes/jobs.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { register } = require("../controllers/auth");
+const router = express.Router();
+
+const {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/jobs");
+
+router.route("/").post(createJob).get(getAllJobs);
